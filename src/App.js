4e7e9fab91c93164ab3d4d0ad44c8fae7baec45f
DESKTOP-2PA9VA1\DELL <ibrahim15-14295@diu.edu.bd>
2022-11-25 18:27:27 +0600

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Navbar from "./Components/Navbar"
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Home/>
+      <home/>
+      
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import"./Home.css";
+import Facilites from "./Facilites";
+import Contract from "./Contract";
+import faq1 from "./faq1";
+function Home(){
+    return (
+    <>
+    <div className="home">
+        <Facilites></Facilites>
+        <Contract></Contract>
+        <faq1></faq1>
+
+    </div>
+    
+    
+    </>
+    
+    );
+}
+export default Home;
